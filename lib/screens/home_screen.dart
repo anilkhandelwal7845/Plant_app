0e69e717a188d6509ea051183618b3f4b91af79d
Anil Khandelwal <anilkhandelwal7845d@gmail.com>
2023-06-02 12:05:47 +0530

    inital ..

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/color/constant.dart';
-import 'package:plant_app/screens/btn.dart';
 import 'package:plant_app/screens/text.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -174,7 +173,73 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ],
+            ) ,
+          
+          Column(
+              children: [
+                Container(
+                  height: 250,
+                  width: 160,
+                  margin: EdgeInsets.only(top: 25),
+                  decoration: BoxDecoration(
+                    color: Colors.white ,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0)),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10.0),
+                              topLeft: Radius.circular(10.0)),
+                        ),
+                        height: 200,
+                        width: 160,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 5,) ,
+                                Text(
+                                  "SAMANTHA",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black),
+                                ),SizedBox(height: 5,) ,
+                                Text(
+                                  "INDIAN",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: kPrimearyColor),
+                                )
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, right: 4),
+                            child: Text(
+                              '\u{20B9}${900}',
+                              style: TextStyle(
+                                  fontSize: 20, color: kPrimearyColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             )
+          
           ],
         ),
       ),
