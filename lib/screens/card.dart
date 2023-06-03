57ba5ee59049cd83b5dbc633552946e5a088fe70
Anil Khandelwal <anilkhandelwal7845d@gmail.com>
2023-06-03 23:21:48 +0530

    overview done first screen .

--- lib/screens/card.dart
+++ lib/screens/card.dart
@@ -1,83 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/color/constant.dart';
+import 'package:plant_app/screens/details_screen.dart';
 
 class PlantCard extends StatelessWidget {
   final String image, title, country;
   final int price;
-  final Function press;
+ 
+  
   const PlantCard(
       {super.key,
       required this.country,
       required this.image,
       required this.title,
       required this.price,
-      required this.press});
+      
+      });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        left: 10,
-        right: 5,
-        top: 20,
-      ),
-      height: MediaQuery.of(context).size.height /3,
-      width: MediaQuery.of(context).size.width / 3,
-
-      // color: Colors.red,
-      child: Column(
-        children: [
-          Image.network(
-            image,
-            fit: BoxFit.cover,
-          ),
-          GestureDetector(
-            onTap: () => press,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(13),
-                      bottomRight: Radius.circular(13)),
-                  color: Colors.grey,
-                  boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 0),
-                        blurRadius: 20,
-                        color: kPrimearyColor.withOpacity(0.23)),
-                  ]),
-              child: Row(
-                children: [
-                  RichText(
-                    text: TextSpan(children: [
-                      TextSpan(
-                          text: "$title\n".toUpperCase(),
-                          style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                              color: kPrimearyColor.withOpacity(0.7))),
-                      TextSpan(
-                          text: country.toUpperCase(),
-                          style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.w400,
-                              color: kPrimearyColor.withOpacity(0.7)))
+    return InkWell(
+       onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailsScreen()));
+            } ,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: 10,
+          right: 5,
+          top: 20,
+        ),
+        height: 300,
+        width: MediaQuery.of(context).size.width * 0.35,
+        child: Column(
+          children: [
+    
+            Container(
+              height: 220,
+               width: MediaQuery.of(context).size.width * 0.35,
+            
+              child: Image.network(
+              image,
+              fit: BoxFit.cover,
+            ), 
+            ) ,
+    
+           
+            GestureDetector(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(13),
+                        bottomRight: Radius.circular(13)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 0),
+                          blurRadius: 20,
+                          color: kPrimearyColor.withOpacity(0.23)),
                     ]),
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Text(
-                      '\u{20B9}$price',
-                      style: TextStyle(
-                          color: kPrimearyColor, fontWeight: FontWeight.bold),
+                child: Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: "$title\n".toUpperCase(),
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: kPrimearyColor.withOpacity(0.7))),
+                        TextSpan(
+                            text: country.toUpperCase(),
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400,
+                                color: kPrimearyColor.withOpacity(0.7)))
+                      ]),
                     ),
-                  ),
-                ],
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15 ,left: 2 ,right: 1),
+                      child: Text(
+                        '\u{20B9}$price',
+                        style: TextStyle(
+                          fontSize: 15,
+                            color: kPrimearyColor, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plant_app/color/constant.dart';
 import 'package:plant_app/screens/btn.dart';
 import 'package:plant_app/screens/card.dart';
+import 'package:plant_app/screens/details_screen.dart';
 import 'package:plant_app/screens/featuredcard.dart';
 import 'package:plant_app/screens/text.dart';
 
@@ -113,49 +114,82 @@ class _HomeScreenState extends State<HomeScreen> {
                 //Here Add Btn
               ],
             ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  PlantCard(
+                      country: "India",
+                      image:
+                          'https://www.plantshop.me/media/product/Kentia_Palm_-a-_plantshopme.jpg',
+                      title: "Kentia Palm",
+                      price: 2000,
+                     ),
+                  PlantCard(
+                      country: "India",
+                      image:
+                          'https://cdn.shopify.com/s/files/1/0479/9089/8856/products/V.G.FicusPlantN.Stick6FEET.jpg?v=1618553049',
+                      title: "Kentia Palm",
+                      price: 200,
+                      
+                      ),
+                  PlantCard(
+                      country: "India",
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwoCcjV5qwFIHUW23VLXcTHzf9ZVFTVGwOj7mXA1xwUA&usqp=CAU&ec=48665699',
+                      title: "Kentia Palm",
+                      price: 400,
+                      
+                     
+                      ),
+                  PlantCard(
+                      country: "India",
+                      image:
+                          'https://target.scene7.com/is/image/Target/GUEST_cf6a8c1d-a2cb-4a8a-aef6-ca3f3b9118fc',
+                      title: "Kentia Palm",
+                      price: 900,
+                    
+                      ),
 
+                       PlantCard(
+                      country: "India",
+                      image:
+                          'https://ii2.pepperfry.com/media/catalog/product/g/r/494x544/green-fabric-real-touch-artificial-fiddle-leaf-fig-plant-without-pot-by-fourwalls-green-fabric-real--ud63n9.jpg',
+                      title: "Kentia Palm",
+                      price: 900,
+                     
+                      ),
 
-SingleChildScrollView(
-  scrollDirection: Axis.horizontal,
-  child:   Row(
-  
-    children: [
-  
-  
-  
-  PlantCard(country: "India", image: 'https://www.plantshop.me/media/product/Kentia_Palm_-a-_plantshopme.jpg', title: "Kentia Palm", price: 2000, press:(){
-  
-    print("-------------------");
-  
-  }) ,
-  
-  PlantCard(country: "India", image: 'https://www.plantshop.me/media/product/Kentia_Palm_-a-_plantshopme.jpg', title: "Kentia Palm", price: 200, press:(){
-  
-    print("-------------------");
-  
-  }) ,
-  
-  PlantCard(country: "India", image: 'https://www.plantshop.me/media/product/Kentia_Palm_-a-_plantshopme.jpg', title: "Kentia Palm", price: 400, press:(){
-  
-    print("-------------------");
-  
-  }) ,
-  
-  PlantCard(country: "India", image: 'https://www.plantshop.me/media/product/Kentia_Palm_-a-_plantshopme.jpg', title: "Kentia Palm", price: 900, press:(){
-  
-    print("-------------------");
-  
-  }) ,
+                       PlantCard(
+                      country: "India",
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNFoBKW1hJTFFUoCWpH1JKqEVmcj8gCytGhro_NZzoTuYlfBGJEiESux-0YT02dtJqVpDXLbbS7Nk&usqp=CAU&ec=48665699',
+                      title: "Kentia Palm",
+                      price: 900,
+                     
+                      ),
 
+                       PlantCard(
+                      country: "India",
+                      image:
+                          'https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&imageId=1363170-847__1&recipeName=350',
+                      title: "Kentia Palm",
+                      price: 900,
+                      
+                      ),
 
-  
-    ],
-  
-  ),
-) ,
-
-
- Row(
+                       PlantCard(
+                      country: "India",
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi6KBzlMfuH8drn0PwiGKd8VmmgMsQYQUZocEFGFfqNh3TUaHbk0rX3jRZQbTps_hjUzL0ilzgkZ8&usqp=CAU&ec=48665699',
+                      title: "Kentia Palm",
+                      price: 900,
+                      
+                      ),
+                ],
+              ),
+            ),
+            Row(
               children: [
                 MainText(text: "Featured Plants"),
                 Spacer(),
@@ -164,31 +198,26 @@ SingleChildScrollView(
                 //Here Add Btn
               ],
             ),
-          
-
-SingleChildScrollView(
-  scrollDirection: Axis.horizontal,
-  child:   Row(
-  
-    children: [
-
-      
-
-
-FeaturedCard(image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCM9CxdlhB1zvF6a49RM5BTlIJ68OKhWRReotUTmqP9beijUlmAnEyycBnpxUb1ef6ZgBlo65XK9Y&usqp=CAU&ec=48665699') ,
-FeaturedCard(image: 'https://hips.hearstapps.com/hmg-prod/images/indoor-plants-in-pots-1675181865.jpeg?crop=1.00xw:0.752xh;0,0.123xh&resize=1200:*') ,
-FeaturedCard(image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkk8hXYMdvMS1DzJrRtjODhyIU1C329U7AXC1gxinWkcZlKOuFx8tpuaMt4slOXlBK64VkDDwi4Nc&usqp=CAU&ec=48665699') ,
-FeaturedCard(image: 'https://jayscotts.com/wp-content/uploads/2021/10/spacejoy-IH7wPsjwomc-unsplash-edited-scaled-2.jpg')
-  
-      
-  
-    ],
-  
-  ),
-)
-
-                   
- ],
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  FeaturedCard(
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCM9CxdlhB1zvF6a49RM5BTlIJ68OKhWRReotUTmqP9beijUlmAnEyycBnpxUb1ef6ZgBlo65XK9Y&usqp=CAU&ec=48665699'),
+                  FeaturedCard(
+                      image:
+                          'https://hips.hearstapps.com/hmg-prod/images/indoor-plants-in-pots-1675181865.jpeg?crop=1.00xw:0.752xh;0,0.123xh&resize=1200:*'),
+                  FeaturedCard(
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkk8hXYMdvMS1DzJrRtjODhyIU1C329U7AXC1gxinWkcZlKOuFx8tpuaMt4slOXlBK64VkDDwi4Nc&usqp=CAU&ec=48665699'),
+                  FeaturedCard(
+                      image:
+                          'https://jayscotts.com/wp-content/uploads/2021/10/spacejoy-IH7wPsjwomc-unsplash-edited-scaled-2.jpg')
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
